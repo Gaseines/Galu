a013260dc95f14f60cfc82110a7a354271accc25
Gaseines <gaseinesdzn@hotmail.com>
2024-10-24 20:49:43 -0300

    Update Enunciado.js

--- galu/src/components/subComponents/Enunciado.js
+++ galu/src/components/subComponents/Enunciado.js
@@ -4,7 +4,7 @@ import styles from './Enunciado.module.css'
 function Enunciado() {
     return(
         <div className={styles.container_enunciado}>
-            <h1>Gabriel e <span className={styles.marcado_lavanda}>Luiza</span></h1>
+            <h1>Gabriel e <span>Luiza</span></h1>
             <p>A vida pode estar um <span className={styles.marcado_amarelo}>caos</span></p>
             <p>mas a <span className={styles.marcado_amarelo}>organização</span> está aqui</p>
         </div>
